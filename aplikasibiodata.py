fc9b1fae93fd7179a3c8e6c234857dcd990b0faf
Maximillian.H <114460381+SabxZero@users.noreply.github.com>
2022-10-05 22:37:07 +0700

    Add files via upload

---
+++ aplikasibiodata.py
@@ -0,0 +1,17 @@
+# nama = input("masukan nama anda! ")
+# nim = input("masukan nim anda! ")
+# kelas = input("masukan kelas anda! ")
+# prodi = input("masukan prodi anda! ")
+# print ("--------------------")
+# print("nama anda adalah ", nama)
+# print("nim anda adalah", nim)
+# print("kelas anda adalah", kelas)
+# print("prodi anda adalah", prodi)
+
+
+Nama = ("maximillian")
+kelas = ("a")
+jurusan = ("teknik informatika")
+quote = ("just do it than you dont at all")
+
+print ("nama = ", Nama, "\n", "kelas =", kelas, "\n", "jurusan = ", jurusan, "\n", "quote = ", quote)
