150e3a9735f87b17bcc16a06a4a719cdbb107bdb
Maximillian.H <114460381+SabxZero@users.noreply.github.com>
2022-10-14 10:33:08 +0700

    Add files via upload

---
+++ belajar2.py
@@ -0,0 +1,33 @@
+# a = int(input("alas = "))
+# t = int(input("tinggi = "))
+# rumus = (1/2) * a * t
+# print("hasil = ", int(rumus))
+
+# password = int(input("password = "))
+# isipasword = int(input("isi password = "))
+# hasil = isipasword is password 
+# print ("password is = ",bool(hasil))
+
+# nama = input("nama = ")
+# kelas = input("nama = ")
+# prodi = input("prodi = ")
+# print ("namaku adalah", nama, "aku di kelas", kelas, "prodiku", prodi )
+
+# nama = input("nama barang = ")
+# a = int(input("price = "))
+# b = (10/100)
+# rumus = a * b
+# rumus2 = rumus + a
+# print ("harga jual", nama, "=", rumus2)
+# print ("keuntungan = ", rumus)
+
+#simple
+nama = input("nama barang = ")
+b = int(input("banyak barang = "))
+a = int(input("price = "))
+rumus = ((a/100) * 10) + a
+rumus2 = ((a/100) * 10)
+rumus3 = (((a/100) * 10) + a) * b
+rumus4 = ((a/100) * 10) * b
+print ("harga jual/barang", nama, "=", rumus, "\n" "keuntungan/ barang =", rumus2)
+print ("harga jual seluruh produk = ", rumus3, "\n", "keuntungan semua barang =", rumus4 )
